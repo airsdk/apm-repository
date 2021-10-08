72db2efb48b6090dcaaea498cf18c142d8a9ba9b
marchbold <marchbold@users.noreply.github.com>
2021-10-08 15:09:58 +1000

    Implemented version ranges and new dependency model allowing for variable version dependencies

--- pages/api/packages/[packageId]/[version]/index.ts
+++ pages/api/packages/[packageId]/[version]/index.ts
@@ -2,8 +2,26 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import prisma from "../../../../../lib/prisma";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const packageId = req.query.packageId;
-  const version = req.query.version;
+  const packageId: string = String(req.query.packageId);
+  const version: string = String(req.query.version);
+  const includePrerelease: boolean =
+    req.query.includePrerelease === undefined
+      ? false
+      : req.query.includePrerelease == "true";
+
+  // Only populate this for a version query as we allow direct access to any version regardless of status
+  var excludedStatusValues: string[] = [];
+  var versionQuery: string = version;
+
+  // Check for a version range query
+  if (versionQuery.indexOf("x") >= 0) {
+    // Remove x to get something like "4.5." from "4.5.x"
+    versionQuery = versionQuery.substring(0, versionQuery.indexOf("x"));
+
+    // This is a query so limit status
+    if (!includePrerelease) excludedStatusValues.push("prerelease");
+    excludedStatusValues.push("discontinued");
+  }
 
   const packageData = await prisma.package.findFirst({
     where: {
@@ -26,11 +44,18 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
           parameters: true,
         },
         where: {
-          version: String(version),
+          published: true,
+          version: {
+            startsWith: versionQuery,
+          },
+          status: {
+            notIn: excludedStatusValues,
+          },
         },
         orderBy: {
           publishedAt: "desc",
         },
+        take: 1,
       },
     },
   });
